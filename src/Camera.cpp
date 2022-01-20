267d4e2bbb8d002249c8a98483913e22deec06cc
fire-walk-with-me <mrc.e.bra@gmail.com>
2022-01-20 12:10:05 +0100

    Update Camera.cpp

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -28,8 +28,8 @@ void Camera::moveTo(const vec3f& p)
 }
 
 void Camera::move(const vec3f& v)
-{
-	position += v;
+{	
+	position += v * rotation.get_3x3().inverse();
 }
 
 mat4f Camera::get_ProjectionMatrix()
@@ -43,7 +43,7 @@ mat4f Camera::get_WorldToViewMatrix()
 	// the View-to-World transform is T(p)*R (for a first-person style camera).
 	//
 	// World-to-View then is the inverse of T(p)*R;
-	//		inverse(T(p)*R) = inverse(R)*inverse(T(p)) = transpose(R)*T(-p)
+	// inverse(T(p)*R) = inverse(R)*inverse(T(p)) = transpose(R)*T(-p)
 	// Since now there is no rotation, this matrix is simply T(-p)
 
 	worldToView = mat4f::translation(position) * rotation;
